5dd33b461c37e16aae0f090eb40c2be1229ab7ee
23kimh <53035685+23kimh@users.noreply.github.com>
2019-07-18 16:50:12 +0900

    Create baekjoon-problem-2588.py
    https://www.acmicpc.net/problem/2588

---
+++ BaekJoon - Python Practice/baekjoon-problem-2588.py
@@ -0,0 +1,27 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ### another baekjoon problem #2588
+>>> n = int(input())
+472
+>>> m = input()
+385
+>>> type(n)
+<class 'int'>
+>>> type(m)
+<class 'str'>
+>>> m[0]
+'3'
+>>> m[1]
+'8'
+>>> m[2]
+'5'
+>>> print(n*int(m[2]))
+2360
+>>> print(n*int(m[1]))
+3776
+>>> print(n*int(m[0]))
+1416
+>>> print(n*int(m))
+181720
+>>> 
